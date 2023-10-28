fef34fec8aa2dff1a021f61175cf368074569b12
Anne Huynh <huynh.anne@gmail.com>
2023-10-28 15:21:52 -0700

    Added signature and css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import WeatherSearch from "./WeatherSearch";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
+      <div className="Weather-Wrapper">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+      </div>
+      <div className="Signature">
+        <a
+          href="https://github.com/annetheanne/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by{" "}
+        <a href="https://annehuynh.com" target="_blank" rel="noreferrer">
+          Anne Huynh
+        </a>
+      </div>
     </div>
   );
 }

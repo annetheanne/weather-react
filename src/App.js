7f6764c5da11902c343dae47b1fdbef82e6bebe7
Anne Huynh <huynh.anne@gmail.com>
2023-11-06 15:58:44 -0800

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import WeatherSearch from "./WeatherSearch";
 function App() {
   return (
     <div className="App">
-      <div className="Weather-Wrapper">
-        <h1>Weather App</h1>
-        <WeatherSearch />
-      </div>
-      <div className="Signature">
-        <a
-          href="https://github.com/annetheanne/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code
-        </a>{" "}
-        by{" "}
-        <a href="https://annehuynh.com" target="_blank" rel="noreferrer">
-          Anne Huynh
-        </a>
+      <div className="container">
+        <div className="Weather-Wrapper">
+          <h1>Weather App</h1>
+          <WeatherSearch />
+        </div>
+        <div className="Signature">
+          <a
+            href="https://github.com/annetheanne/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          by{" "}
+          <a href="https://annehuynh.com" target="_blank" rel="noreferrer">
+            Anne Huynh
+          </a>
+        </div>
       </div>
     </div>
   );
